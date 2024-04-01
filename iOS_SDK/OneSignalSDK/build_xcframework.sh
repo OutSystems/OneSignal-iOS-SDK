87742f17cf3e5dbd004e870f16230243bde2043b
OS-ricardomoreirasilva <ricardo.moreira.silva@outsystems.com>
2024-04-01 11:42:38 +0100

    chore: Make it work with our stack
    Apply the necessary changes so that the SDK can be built using XCode 14 and 15, the ones used by the current support MABS versions.
    Update the `build_xcframework.sh` bash script by removing the unused Mac Catalyst build script and organising it similarly to the already existing scripts.
    
    References: https://outsystemsrd.atlassian.net/browse/RMET-3276

--- iOS_SDK/OneSignalSDK/build_xcframework.sh
+++ iOS_SDK/OneSignalSDK/build_xcframework.sh
@@ -15,23 +15,35 @@ SIMULATOR_ARCHIVE_PATH="${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}/simulator.xcarch
 
 IOS_DEVICE_ARCHIVE_PATH="${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}/iOS.xcarchive"
 
-CATALYST_ARCHIVE_PATH="${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}/catalyst.xcarchive"
-
 rm -rf "${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}"
 echo "Deleted ${FRAMEWORK_FOLDER_NAME}"
 mkdir "${FRAMEWORK_FOLDER_NAME}"
 echo "Created ${FRAMEWORK_FOLDER_NAME}"
 echo "Archiving ${FRAMEWORK_NAME}"
 
-xcodebuild archive ONLY_ACTIVE_ARCH=NO -scheme ${BUILD_SCHEME} -destination="generic/platform=iOS Simulator" -archivePath "${SIMULATOR_ARCHIVE_PATH}" -sdk iphonesimulator SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
+xcodebuild archive ONLY_ACTIVE_ARCH=NO \
+	-scheme ${BUILD_SCHEME} \
+	-destination="generic/platform=iOS Simulator" \
+	-archivePath "${SIMULATOR_ARCHIVE_PATH}" \
+	-sdk iphonesimulator \
+	SKIP_INSTALL=NO \
+	BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
 
-xcodebuild archive -scheme ${BUILD_SCHEME} -destination="generic/platform=iOS" -archivePath "${IOS_DEVICE_ARCHIVE_PATH}" -sdk iphoneos SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
+xcodebuild archive \
+	-scheme ${BUILD_SCHEME} \
+	-destination="generic/platform=iOS" \
+	-archivePath "${IOS_DEVICE_ARCHIVE_PATH}" \
+	-sdk iphoneos \
+	SKIP_INSTALL=NO \
+	BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
 
-xcodebuild archive -scheme ${BUILD_SCHEME} -destination='generic/platform=macOS,variant=Mac Catalyst' -archivePath "${CATALYST_ARCHIVE_PATH}" SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
+xcodebuild -create-xcframework \
+	-framework ${SIMULATOR_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework \
+	-framework ${IOS_DEVICE_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework \
+	-output "${FRAMEWORK_PATH}"
 
-xcodebuild -create-xcframework -framework ${SIMULATOR_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework -framework ${IOS_DEVICE_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework -framework ${CATALYST_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework -output "${FRAMEWORK_PATH}"
+codesign --timestamp -v --sign 'OutSystems Inc. (S25XN959HW)' "${FRAMEWORK_PATH}"
 
 rm -rf "${SIMULATOR_ARCHIVE_PATH}"
 rm -rf "${IOS_DEVICE_ARCHIVE_PATH}"
-rm -rf "${CATALYST_ARCHIVE_PATH}"
 open "${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}"
