e3d80815014c8fa9cfc2ad10280b2addc27fc3ae
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2026-04-20 12:19:21 +0100

    chore: 4th test

--- iOS_SDK/OneSignalSDK/Source/OneSignal.m
+++ iOS_SDK/OneSignalSDK/Source/OneSignal.m
@@ -926,7 +926,7 @@ void onesignal_Log(ONE_S_LOG_LEVEL logLevel, NSString* message) {
     }
 }
 
-//presents the settings page to control/customize push notification settings; test 3
+//presents the settings page to control/customize push notification settings; test 4
 + (void)presentAppSettings {
     
     //only supported in 10+
